79db7ebc23bebbf62edafd639306b2c3602fc2ef
ondra@dynawest.cz <ondra@dynawest.cz@d2c96cac-2364-11df-9cb1-5138f70e336d>
2010-04-09 19:07:17 +0000

    Added WicketTest/Chat

---
+++ trunk/WicketTest/Chat/src/main/java/cz/dynawest/wicket/chat/ChatPage.java
@@ -0,0 +1,28 @@
+
+package cz.dynawest.wicket.chat;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.wicket.Page;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+
+/**
+ *
+ * @author Ondrej Zizka
+ */
+public class ChatPage extends Page {
+
+	private List<String> messages = new ArrayList();
+
+	public ChatPage() {
+
+		this.add( new ListView("messages", this.messages) {
+			@Override protected void populateItem(ListItem item) {
+				throw new UnsupportedOperationException("Not supported yet.");
+			}
+		});
+
+	}
+
+}// class
---
+++ trunk/WicketTest/Chat/src/main/java/cz/dynawest/wicket/chat/ChatWicketApp.java
@@ -0,0 +1,18 @@
+
+package cz.dynawest.wicket.chat;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ *
+ * @author Ondrej Zizka
+ */
+public class ChatWicketApp extends WebApplication {
+
+	@Override
+	public Class<? extends Page> getHomePage() {
+		return new ChatPage();
+	}
+
+}// class
