876f3f0280487a64bac8f9c23ea253658b8919dd
ondra <ondra@dynawest.cz>
2010-04-09 19:07:17 +0000

    Added WicketTest/Chat

---
+++ WicketTest/Chat/src/main/java/cz/dynawest/wicket/chat/ChatData.java
@@ -0,0 +1,15 @@
+
+package cz.dynawest.wicket.chat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Ondrej Zizka
+ */
+public class ChatData {
+
+	List<String> messages = new ArrayList();
+
+}// class
