c2220942b3d562fab29ffe09926812a3495eac93
ondra@dynawest.cz <ondra@dynawest.cz@d2c96cac-2364-11df-9cb1-5138f70e336d>
2010-05-03 10:29:02 +0000

    Added texy page - PathUrlCodingStrategy testbed.

--- trunk/WicketTry/wicket-quickstart/src/main/java/cz/dw/test/WicketApplication.java
+++ trunk/WicketTry/wicket-quickstart/src/main/java/cz/dw/test/WicketApplication.java
@@ -1,9 +1,13 @@
 package cz.dw.test;
 
-import cz.dw.test.gb.GuestBook;
 import cz.dw.test.listinlist.CreatePerson;
-import cz.dw.test.pages.Page1;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleTrigger;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
@@ -13,11 +17,43 @@ import org.apache.wicket.protocol.http.WebApplication;
 public class WicketApplication extends WebApplication
 {
 
+  Scheduler scheduler;
+
   /**
    * Constructor
    */
   public WicketApplication() {
   }
+
+  // Init
+  @Override
+  protected void init() {
+    System.out.println( "---- init() ----" );
+    try{
+      this.scheduler = StdSchedulerFactory.getDefaultScheduler();
+      scheduler.start();
+      JobDetail job = new JobDetail("job1", "group1", MyJobClass.class);
+      Trigger trigger = new SimpleTrigger("trigger1", "group1", 10, 1000*2);
+      scheduler.scheduleJob(job, trigger);
+    } catch( SchedulerException se ) {
+        se.printStackTrace();
+    }
+  }
+
+
+  // Shutdown
+  @Override
+  protected void onDestroy() {
+    System.out.println( "---- onDestroy() ----" );
+    try {
+      scheduler.shutdown();
+    }
+    catch( SchedulerException ex ) {
+      ex.printStackTrace();
+    }
+  }
+
+
 
 
   /**
