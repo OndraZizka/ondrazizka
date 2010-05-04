9c63f788c07c141be252aa96122980a74679d6bb
ondra <ondra@dynawest.cz>
2010-05-04 01:57:10 +0000

    * Removed quartz tries.

--- WicketTry/wicket-quickstart/src/main/java/cz/dw/test/WicketApplication.java
+++ WicketTry/wicket-quickstart/src/main/java/cz/dw/test/WicketApplication.java
@@ -1,13 +1,8 @@
 package cz.dw.test;
 
-import cz.dw.test.listinlist.CreatePerson;
+import cz.dw.test.texy.ShowPathPage;
 import org.apache.wicket.protocol.http.WebApplication;
-import org.quartz.JobDetail;
 import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.SimpleTrigger;
-import org.quartz.Trigger;
-import org.quartz.impl.StdSchedulerFactory;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
@@ -29,15 +24,6 @@ public class WicketApplication extends WebApplication
   @Override
   protected void init() {
     System.out.println( "---- init() ----" );
-    try{
-      this.scheduler = StdSchedulerFactory.getDefaultScheduler();
-      scheduler.start();
-      JobDetail job = new JobDetail("job1", "group1", MyJobClass.class);
-      Trigger trigger = new SimpleTrigger("trigger1", "group1", 10, 1000*2);
-      scheduler.scheduleJob(job, trigger);
-    } catch( SchedulerException se ) {
-        se.printStackTrace();
-    }
   }
 
 
@@ -45,12 +31,6 @@ public class WicketApplication extends WebApplication
   @Override
   protected void onDestroy() {
     System.out.println( "---- onDestroy() ----" );
-    try {
-      scheduler.shutdown();
-    }
-    catch( SchedulerException ex ) {
-      ex.printStackTrace();
-    }
   }
 
 
@@ -64,7 +44,7 @@ public class WicketApplication extends WebApplication
     //return Page1.class;
     //return GuestBook.class;
     //return CreatePerson.class;
-    return CreatePerson.class;
+    return ShowPathPage.class;
   }
 
 
