0acce2d02057da1f89f0b30626da1ce2bf8649c2
ondra@dynawest.cz <ondra@dynawest.cz@d2c96cac-2364-11df-9cb1-5138f70e336d>
2010-05-09 02:45:50 +0000

    * Yet to integrate JTexy...

--- trunk/WicketTry/wicket-quickstart/src/main/java/cz/dw/test/texy/BaseLayoutPage.java
+++ trunk/WicketTry/wicket-quickstart/src/main/java/cz/dw/test/texy/BaseLayoutPage.java
@@ -2,10 +2,12 @@
 package cz.dw.test.texy;
 
 
+import cz.oz.MenuPanel;
 import java.util.*;
 import java.util.logging.*;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.include.Include;
 
 
 /**
@@ -21,6 +23,10 @@ public class BaseLayoutPage extends WebPage
 
     add( new Header( "header" ));
     //add( new Header( "content" )); // The rest is done in the inherited classes.
+
+    add( new MenuPanel( "menu" ) );
+    add( new Include( "donate", "static/DonatePanel.html" ) );
+    add( new Include( "counters", "static/CountersPanel.html" ) );
   }
 
   
---
+++ trunk/WicketTry/wicket-quickstart/src/main/java/cz/oz/MenuPanel.java
@@ -0,0 +1,21 @@
+
+package cz.oz;
+
+
+import java.util.logging.*;
+import org.apache.wicket.markup.html.panel.Panel;
+
+
+/**
+ *
+ * @author Ondrej Zizka
+ */
+public class MenuPanel extends Panel
+{
+  private static final Logger log = Logger.getLogger( MenuPanel.class.getName() );
+
+  public MenuPanel( String id ) {
+    super( id );
+  }
+
+}// class MenuPanel
