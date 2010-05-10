5d16aad76e5748f1660f0b1e6c80f1fe275eda5b
ondra <ondra@dynawest.cz>
2010-05-10 13:09:45 +0000

    * Works with embedded Jetty, too. * Include replaced with normal Panels.

---
+++ WicketTry/wicket-quickstart/src/main/java/cz/oz/DonatePanel.java
@@ -0,0 +1,21 @@
+
+package cz.oz;
+
+
+import java.util.logging.*;
+import org.apache.wicket.markup.html.panel.Panel;
+
+
+/**
+ *
+ * @author Ondrej Zizka
+ */
+public class DonatePanel extends Panel
+{
+  private static final Logger log = Logger.getLogger( DonatePanel.class.getName() );
+
+  public DonatePanel( String id ) {
+    super( id );
+  }
+
+}// class MenuPanel
