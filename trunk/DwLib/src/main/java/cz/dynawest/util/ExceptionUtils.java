e87e8000304d8f9bbbfb232d47a0ec4748cdfd1a
ondra@dynawest.cz <ondra@dynawest.cz@d2c96cac-2364-11df-9cb1-5138f70e336d>
2010-08-08 20:24:28 +0000

    1.02 ExceptionUtils

---
+++ trunk/DwLib/src/main/java/cz/dynawest/util/ExceptionUtils.java
@@ -0,0 +1,56 @@
+
+package cz.dynawest.util;
+
+
+/**
+ *
+ * @author Ondrej Zizka
+ */
+public class ExceptionUtils
+{
+
+
+  /**
+   * @returns the most nested cause.
+   */
+  public static Throwable getRootCause( Throwable ex ){
+
+    //return null == ex.getCause() ? getRootCause( ex.getCause() ) : ex;
+
+    int i = 0;
+
+    Throwable next;
+    while( null != (next = ex.getCause()) ){
+      ex = next;
+      
+      // Loop-prevention.
+      if( ++i > 100 )
+        break;
+    }
+
+    return ex;
+
+  }
+
+
+  /**
+   * @returns true if some of the nested causes are of the class @cls.
+   */
+  public static boolean hasCause( Throwable ex, Class cls ){
+    
+    int i = 0;
+
+    while( null != ex ) {
+      if( cls.isInstance( ex ) )
+        return true;
+      ex = ex.getCause();
+
+      // Loop-prevention.
+      if( ++i > 100 )
+        break;
+    }
+    
+    return false;
+  }
+
+}// class ExceptionUtils
