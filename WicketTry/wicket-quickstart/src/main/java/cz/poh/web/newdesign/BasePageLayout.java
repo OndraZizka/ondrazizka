5b130f7d66faa40f26ebec3407c20de265d12441
ondra <ondra@dynawest.cz>
2011-01-04 12:58:33 +0000

    New design

---
+++ WicketTry/wicket-quickstart/src/main/java/cz/poh/web/newdesign/BasePageLayout.java
@@ -0,0 +1,22 @@
+
+package cz.poh.web.newdesign;
+
+
+import java.io.Serializable;
+import java.util.logging.*;
+import org.apache.wicket.markup.html.WebPage;
+
+
+/**
+ *
+ * @author Ondrej Zizka
+ */
+public class BasePageLayout extends WebPage implements Serializable
+{
+  private static final Logger log = Logger.getLogger( BasePageLayout.class.getName() );
+
+
+  
+
+
+}// class BasePageLayout
