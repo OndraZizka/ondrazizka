500a063630283894650449ba324a4ec359de86a5
ondra@dynawest.cz <ondra@dynawest.cz@d2c96cac-2364-11df-9cb1-5138f70e336d>
2011-01-05 04:41:23 +0000

    IR - home page extracted into panels.

---
+++ trunk/WicketStack/src/main/java/cz/poh/web/newdesign/AboutAtHomePagePanel.java
@@ -0,0 +1,17 @@
+
+package cz.poh.web.newdesign;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ *
+ * @author ondra
+ */
+public final class AboutAtHomePagePanel extends Panel
+{
+
+    public AboutAtHomePagePanel( String id ) {
+        super(id);
+    }
+
+}// class AboutAtHomePagePanel
---
+++ trunk/WicketStack/src/main/java/cz/poh/web/newdesign/PersonalMenuPanel.java
@@ -0,0 +1,17 @@
+
+package cz.poh.web.newdesign;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+/**
+ *
+ * @author ondra
+ */
+public final class PersonalMenuPanel extends Panel
+{
+
+    public PersonalMenuPanel( String id ) {
+        super(id);
+    }
+
+}// class PersonalMenu
