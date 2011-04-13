8e0137273dad529dca2ddeb557c3693b5342e8d1
ondra@dynawest.cz <ondra@dynawest.cz@d2c96cac-2364-11df-9cb1-5138f70e336d>
2011-04-13 02:55:59 +0000

    Trying to get JNDI working.

--- trunk/bots/JawaBot/branches/2.0/web/src/main/java/org/jboss/jawabot/web/RunInJetty.java
+++ trunk/bots/JawaBot/branches/2.0/web/src/main/java/org/jboss/jawabot/web/RunInJetty.java
@@ -5,15 +5,27 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.EventListener;
+import java.util.Hashtable;
+import javax.naming.InitialContext;
+import javax.naming.Name;
+import javax.naming.NameParser;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 import org.apache.wicket.application.ReloadingClassLoader;
 import org.apache.wicket.protocol.http.ReloadingWicketServlet;
 //import org.jboss.weld.wicket.BeanManagerLookup;
 import org.mortbay.jetty.Server;
+import org.mortbay.jetty.plus.naming.EnvEntry;
+import org.mortbay.jetty.plus.naming.NamingEntryUtil;
 import org.mortbay.jetty.plus.naming.Resource;
 import org.mortbay.jetty.servlet.*;
+import org.mortbay.jetty.webapp.Configuration;
 import org.mortbay.jetty.webapp.WebAppContext;
+import org.mortbay.jetty.webapp.WebXmlConfiguration;
+import org.mortbay.naming.NamingUtil;
 import org.mortbay.xml.XmlConfiguration;
 
 
@@ -145,19 +157,73 @@ public class RunInJetty
 
          // See Jetty examples - FromXmlConfiguration.java
          //String confXml = IOUtils.toString( RunInJetty.class.getResourceAsStream( "/WEB-INF/jetty-env.xml" ) );
-         XmlConfiguration configuration = new XmlConfiguration( RunInJetty.class.getResourceAsStream( "/WEB-INF/jetty-env.xml" ) ); 
-         configuration.configure(ctx);
+         XmlConfiguration confJettyEnv = new XmlConfiguration( RunInJetty.class.getResourceAsStream("/WEB-INF/jetty-env.xml") ); 
+         confJettyEnv.configure(ctx);
          
-         //BeanManagerLookup.getBeanManagerJndiName();
-         Resource resource = 
-            new org.mortbay.jetty.plus.naming.Resource( ctx, "BeanManager",
+         
+         javax.naming.Reference ref = 
                new javax.naming.Reference(
-               javax.enterprise.inject.spi.BeanManager.class.getName(),
-               //"org.jboss.weld.resources.ManagerObjectFactory",
-               //new org.jboss.seam.solder.beanManager.BeanManagerLocator().getBeanManager().,
-               
-               null )
-            );
+                  javax.enterprise.inject.spi.BeanManager.class.getName(),
+                  //"org.jboss.weld.resources.ManagerObjectFactory",
+                  org.jboss.weld.resources.ManagerObjectFactory.class.getName(),
+                  null 
+               );
+
+         //BeanManagerLookup.getBeanManagerJndiName();
+         Resource resource = new org.mortbay.jetty.plus.naming.Resource( ctx, "BeanManager", ref );
+         EnvEntry envEntry = new org.mortbay.jetty.plus.naming.EnvEntry( ctx, "BeanManager", ref, true );
+         
+
+         /*
+         Hashtable<String, String> hashtable = new Hashtable();
+         hashtable.put( "java.naming.factory.initial",  "org.mortbay.naming.InitialContextFactory" );
+         hashtable.put( "java.naming.factory.url.pkgs", "org.mortbay.naming" );
+         new org.mortbay.naming.InitialContextFactory().getInitialContext(hashtable).bind("BeanManager", ref);
+         //new org.mortbay.naming.InitialContextFactory().getInitialContext(hashtable).bind("java:comp/BeanManager", ref);
+         //new org.mortbay.naming.InitialContextFactory().getInitialContext(hashtable).bind("env/BeanManager", ref);
+          */
+
+         InitialContext ic = new InitialContext();
+         NameParser parser = ic.getNameParser("");
+         //Name prefix = NamingEntryUtil.getNameForScope(scope);
+         Name prefix = parser.parse("").add("_");
+
+         //bind the NamingEntry into the context
+         Name namingEntryName = NamingEntryUtil.makeNamingEntryName(parser, "BeanManager");
+         namingEntryName.addAll(0, prefix);
+         String namingEntryNameString = namingEntryName.toString();
+         NamingUtil.bind( ic, namingEntryNameString, envEntry );
+
+         //bind the object as well
+         Name objectName = parser.parse("BeanManager");
+         objectName.addAll(0, prefix);
+         String objectNameString = objectName.toString();
+         NamingUtil.bind(ic, objectNameString, ref);
+         NamingUtil.bind(ic, "BeanManager", ref);
+         NamingUtil.bind(ic, "/BeanManager", ref);
+         NamingUtil.bind(ic, "env/BeanManager", ref);
+         NamingUtil.bind(ic, "/env/BeanManager", ref);
+         NamingUtil.bind(ic, "_/BeanManager", ref);
+         NamingUtil.bind(ic, "_/env/BeanManager", ref);
+         
+
+         // Trying web.xml, but getting NPE at org.mortbay.jetty.webapp.WebXmlConfiguration.initialize(WebXmlConfiguration.java:251)
+         /*
+         WebXmlConfiguration confWebXml = new WebXmlConfiguration(); 
+         String webXmlStr = IOUtils.toString( RunInJetty.class.getResourceAsStream("/WEB-INF/web.xml") );
+         File webXmlTmpFile = new File( FileUtils.getTempDirectory(), "tmpJawaBot-web.xml" );
+         FileUtils.write( webXmlTmpFile, webXmlStr );
+         confWebXml.configure( webXmlTmpFile.getPath() );
+         FileUtils.deleteQuietly( webXmlTmpFile );
+          */
+         
+         // Add WELD listener by hand.
+         ctx.addEventListener( new org.jboss.weld.environment.servlet.Listener() );
+         
+         
+         // DefaultJndiBeanManagerProvider looks for "java:comp/BeanManager";
+         // ServletContainerJndiBeanManagerProvider looks for "java:comp/env/BeanManager";
+         
       }
       catch ( Exception ex ) {
          log.error( "  Error putting BeanManager to JNDI: " + ex, ex );
