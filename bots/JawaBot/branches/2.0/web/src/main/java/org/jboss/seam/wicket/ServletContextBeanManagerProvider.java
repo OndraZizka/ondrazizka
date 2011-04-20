64c2aeec0e7759207d8526ed70e0466803c6d46c
dynawest <dynawest@gmail.com>
2011-04-20 09:18:17 +0000

    From: 	Marek Śmigielski <marek.smigielski@gmail.com> Subject: 	Re: [seam-dev] Running weld + solder + wicket on GAE] Date: 	Wed, 20 Apr 2011 10:22:40 +0200
    You can mine implementation on github (notice that you need also
    service configuration  file to).
    https://github.com/smigielski/wicket/blob/deployOnGAE/impl/src/main/java/org/jboss/seam/wicket/ServletContextBeanManagerProvider.java
    https://github.com/smigielski/wicket/blob/deployOnGAE/impl/src/main/resources/META-INF/services/org.jboss.seam.solder.beanManager.BeanManagerProvider

---
+++ bots/JawaBot/branches/2.0/web/src/main/java/org/jboss/seam/wicket/ServletContextBeanManagerProvider.java
@@ -0,0 +1,35 @@
+package org.jboss.seam.wicket;
+
+import javax.enterprise.inject.spi.BeanManager;
+import javax.servlet.ServletContext;
+
+import org.apache.wicket.Application;
+import org.apache.wicket.protocol.http.WebApplication;
+import org.jboss.seam.solder.beanManager.BeanManagerProvider;
+
+/**
+* A {@link BeanManagerProvider} that looks it up from servletContext. ServletContext is previously resolved from Wicket
+* application.
+*
+* @author Marek Smigielski
+*
+*/
+public class ServletContextBeanManagerProvider implements BeanManagerProvider {
+
+    @Override
+    public int getPrecedence() {
+        return 12;
+    }
+
+    @Override
+    public BeanManager getBeanManager() {
+        ServletContext servletContext = ((WebApplication) Application.get()).getServletContext();
+        if (servletContext != null) {
+            return (BeanManager) servletContext.getAttribute("org.jboss.weld.environment.servlet" + "."
+                    + BeanManager.class.getName());
+        }
+        return null;
+    }
+
+}
+
