af164e31b5cbea5a6b4c50beee05dfc3b0ea882c
dynawest <dynawest@gmail.com>
2011-05-02 10:03:16 +0000

    Layout fixed. + log4j in  web.

--- bots/JawaBot/branches/2.0/web/src/main/java/org/jboss/jawabot/web/_pg/ResourcePage.java
+++ bots/JawaBot/branches/2.0/web/src/main/java/org/jboss/jawabot/web/_pg/ResourcePage.java
@@ -4,6 +4,8 @@ package org.jboss.jawabot.web._pg;
 
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.markup.html.basic.Label;
+import org.jboss.jawabot.JawaBotApp;
+import org.jboss.jawabot.Resource;
 import org.jboss.jawabot.web._base.BaseLayoutPage;
 import org.jboss.jawabot.web._co.ResourceDetailPanel;
 import org.jboss.jawabot.web._co.ResourcesListPanel;
@@ -17,7 +19,7 @@ import org.slf4j.LoggerFactory;
  */
 public class ResourcePage extends BaseLayoutPage
 {
-  private static final Logger log = LoggerFactory.getLogger( HomePage.class );
+  private static final Logger log = LoggerFactory.getLogger( ResourcePage.class );
 
   
    // Page params.
@@ -30,11 +32,11 @@ public class ResourcePage extends BaseLayoutPage
      log.debug(" Page params: " + params );
 
      String resName = params.getString(PARAM_NAME);
-     
-
-     if( resName != null ){
-        add( new Label("heading", "Resource "+resName+" - details"));
-        add( new ResourceDetailPanel( "resPanel", resName ) );
+     Resource res = JawaBotApp.getJawaBot().getResourceManager().getResource( resName );
+ 
+     if( res != null ){
+        add( new Label("heading", "Resource "+res.getName()+" - details"));
+        add( new ResourceDetailPanel( "resPanel", res ) );
      }
      else{
         add( new Label("heading", "List of resources"));
