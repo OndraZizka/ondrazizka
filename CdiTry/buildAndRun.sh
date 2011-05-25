4736edd9f516234421e0347893d63d3b0372df42
ondra <ondra@dynawest.cz>
2011-05-25 15:54:07 +0000

    Updated buildAndRun

--- CdiTry/buildAndRun.sh
+++ CdiTry/buildAndRun.sh
@@ -1,4 +1,2 @@
-mvn clean install
-mvn dependency:copy-dependencies
-cd target
-java -cp CdiTry-1.0-SNAPSHOT.jar:dependency/weld-se-1.1.1.Final.jar org.jboss.qa.test.cditry.AppWeld
+mvn dependency:purge-local-repository clean install dependency:copy-dependencies
+java -cp target/CdiTry-1.0-SNAPSHOT.jar:target/dependency/weld-se-1.1.1.Final.jar org.jboss.qa.test.cditry.AppWeld
