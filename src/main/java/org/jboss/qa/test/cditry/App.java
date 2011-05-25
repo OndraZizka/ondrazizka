6693dd1b095bbebd8243f90d4db57d0af8dc039a
ondra <ondra@dynawest.cz>
2011-05-25 14:39:38 +0000

    Added CdiTry

---
+++ src/main/java/org/jboss/qa/test/cditry/App.java
@@ -0,0 +1,16 @@
+package org.jboss.qa.test.cditry;
+
+import javax.inject.Singleton;
+
+/**
+ * Hello world!
+ *
+ */
+@Singleton
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
