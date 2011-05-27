904d0e91253a4c249951c486de7d01d47d4510cc
ondra <ondra@dynawest.cz>
2011-05-27 01:26:11 +0000

    Changed according to http://www.laliluna.de/articles/2011/01/12/jboss-weld-jpa-hibernate.html

---
+++ CdiJpaTry/src/main/java/org/jboss/qa/test/cditry2/MyTransactional.java
@@ -0,0 +1,22 @@
+
+//package de.laliluna.transactions;
+package org.jboss.qa.test.cditry2;
+
+import javax.interceptor.InterceptorBinding;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * The @InterceptorBinding tells Weld that this annotation can be used with an interceptor.
+ */
+
+@Target({ElementType.METHOD, ElementType.TYPE})
+@InterceptorBinding
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyTransactional {
+
+}// class
+
