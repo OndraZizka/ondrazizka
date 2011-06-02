be20c42561671eaab19259bd087515079a33a0a3
ondra@dynawest.cz <ondra@dynawest.cz@d2c96cac-2364-11df-9cb1-5138f70e336d>
2011-06-02 08:05:08 +0000

    EntityManager tests

---
+++ trunk/bots/JawaBot/branches/2.0/irc/src/main/java/org/jboss/jawabot/irc/ent/IrcUser.java
@@ -0,0 +1,28 @@
+package org.jboss.jawabot.irc.ent;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ *  
+ *  @author Ondrej Zizka
+ */
+@Entity
+public class IrcUser {
+   
+   @Id private long id;
+   
+   private String name;
+   
+   private String host;
+
+   
+   public long getId() { return id; }
+   public void setId(long id) { this.id = id; }
+   public String getName() { return name; }
+   public void setName(String name) { this.name = name; }
+   public String getHost() { return host; }
+   public void setHost(String host) { this.host = host; }
+   
+}// class
+
--- trunk/bots/JawaBot/branches/2.0/irc/src/main/java/org/jboss/jawabot/irc/plugin/logger/LoggerIrcPluginHook.java
+++ trunk/bots/JawaBot/branches/2.0/irc/src/main/java/org/jboss/jawabot/irc/plugin/logger/LoggerIrcPluginHook.java
@@ -1,6 +1,8 @@
 package org.jboss.jawabot.irc.plugin.logger;
 
 import cz.dynawest.util.plugin.PluginEx;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
 import org.jboss.jawabot.irc.IIrcPluginHook;
 import org.jboss.jawabot.irc.IrcPluginException;
 import org.jboss.jawabot.irc.model.IrcMessage;
@@ -8,17 +10,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *  
+ *  Logger plugin - records the IRC messages to database.
+ * 
  *  @author Ondrej Zizka
  */
 public class LoggerIrcPluginHook implements IIrcPluginHook<Object> {
    private static final Logger log = LoggerFactory.getLogger( LoggerIrcPluginHook.class );
 
+   @Inject EntityManager em;
+   
    
    @Override
    public void onMessage(IrcMessage message) throws IrcPluginException {
       log.info(" IRC message: " + message);
-       
+      em.persist( message );
    }
 
    @Override
