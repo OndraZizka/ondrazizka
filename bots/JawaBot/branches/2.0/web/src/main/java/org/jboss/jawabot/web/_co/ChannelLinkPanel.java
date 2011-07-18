8a87b32b596ef8891bc87f5b44f7a4f5f1b539c0
ondra <ondra@dynawest.cz>
2011-07-18 00:44:16 +0000

    Somehow works

---
+++ bots/JawaBot/branches/2.0/web/src/main/java/org/jboss/jawabot/web/_co/ChannelLinkPanel.java
@@ -0,0 +1,47 @@
+
+package org.jboss.jawabot.web._co;
+
+import javax.inject.Inject;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.image.Image;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+import org.jboss.jawabot.state.ent.User;
+import org.jboss.jawabot.usermgr.UserManager;
+import org.jboss.jawabot.web._pg.UserPage;
+
+/**
+ *  
+ *  @author Ondrej Zizka
+ */
+public class ChannelLinkPanel extends Panel {
+   
+   @Inject UserManager userManager;
+
+   public ChannelLinkPanel( String id, final long userId ) {
+      super( id );
+      super.setDefaultModel( new Model( userManager.byID(userId)) );
+   }
+
+   public ChannelLinkPanel( String id, final String userName ) {
+      super( id );
+      super.setDefaultModel( new Model( userManager.byName(userName)) );
+   }
+
+   public ChannelLinkPanel( String id, final User user ) {
+      super( id, new Model(user) );
+   }
+
+   @Override
+   protected void onInitialize() {
+      super.onInitialize();
+      BookmarkablePageLink<User> link = new BookmarkablePageLink<User>( "link", UserPage.class);
+      link.add( new Image( "icoType", "User.gif" ) );
+      link.add( new Label("label", "" + ((User)getDefaultModelObject()).getName() ));
+      add(link);
+   }
+   
+   
+   
+}
