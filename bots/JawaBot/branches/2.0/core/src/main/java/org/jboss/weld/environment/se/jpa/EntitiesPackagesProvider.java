a289d65d191475e06edb8f1a1084424fce9b2ab9
ondra <ondra@dynawest.cz>
2011-07-22 04:10:29 +0000

    Comment on EPP

--- bots/JawaBot/branches/2.0/core/src/main/java/org/jboss/weld/environment/se/jpa/EntitiesPackagesProvider.java
+++ bots/JawaBot/branches/2.0/core/src/main/java/org/jboss/weld/environment/se/jpa/EntitiesPackagesProvider.java
@@ -3,12 +3,15 @@ package org.jboss.weld.environment.se.jpa;
 import java.util.Collection;
 
 /**
- *  Provides a list of packages to search for entities.
+ *  Implementing class provides a list of packages to search for entities.
  * 
  *  @author Ondrej Zizka
  */
 public interface EntitiesPackagesProvider {
 
+   /**
+    *  @returns  A list of packages to search for entities.
+    */
    public Collection<String> getEntityPackages();
    
 }// class
