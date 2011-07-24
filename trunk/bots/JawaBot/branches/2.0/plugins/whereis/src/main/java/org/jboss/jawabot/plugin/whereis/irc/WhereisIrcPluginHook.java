d93201a4ec54277cf3fb9ee92b5a9854a9376046
ondra@dynawest.cz <ondra@dynawest.cz@d2c96cac-2364-11df-9cb1-5138f70e336d>
2011-07-24 20:31:32 +0000

    WhereIs implemented

--- trunk/bots/JawaBot/branches/2.0/plugins/whereis/src/main/java/org/jboss/jawabot/plugin/whereis/irc/WhereisIrcPluginHook.java
+++ trunk/bots/JawaBot/branches/2.0/plugins/whereis/src/main/java/org/jboss/jawabot/plugin/whereis/irc/WhereisIrcPluginHook.java
@@ -1,12 +1,20 @@
 package org.jboss.jawabot.plugin.whereis.irc;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import javax.inject.Inject;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.time.DateUtils;
 import org.jboss.jawabot.irc.ChannelInfoHandler;
 import org.jboss.jawabot.irc.IIrcPluginHook;
 import org.jboss.jawabot.irc.IrcBotProxy;
+import org.jboss.jawabot.irc.IrcPluginException;
 import org.jboss.jawabot.irc.IrcPluginHookBase;
+import org.jboss.jawabot.irc.model.IrcMessage;
 import org.jibble.pircbot.User;
+import org.mortbay.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,10 +33,37 @@ public class WhereisIrcPluginHook extends IrcPluginHookBase implements IIrcPlugi
 
     
     // IRC stuff.
+
+
+    @Override
+    public void onMessage( IrcMessage msg, IrcBotProxy bot ) throws IrcPluginException {
+        if( ! msg.getText().startsWith("whereis") )
+            return;
+        
+        String pattern = StringUtils.removeStart( msg.getText(), "whereis").trim();
+        if( !pattern.contains("*") ){
+            List<SeenInfo> occurences = this.whereIsService.whereIsUser( pattern );
+            if( occurences.size() == 0 ){
+                bot.sendMessage( msg.getUser(), msg.getChannel(), "Sorry, no traces of "+pattern+".");
+            }
+            else{
+                bot.sendMessage( msg.getUser(), msg.getChannel(), this.informAbout( pattern, occurences ) );
+            }
+        }
+    }
+
+
+    @Override
+    public void onPrivateMessage( IrcMessage message, IrcBotProxy bot ) throws IrcPluginException {
+        
+    }
+    
+    
    
     
     @Override
-    public void onJoin( String channel, String user, IrcBotProxy bot  ) {
+    public void onJoin( String channel, String nick, IrcBotProxy bot  ) {
+        this.whereIsService.updateUserInfo( nick, channel, new Date() );
     }
 
 
@@ -38,6 +73,9 @@ public class WhereisIrcPluginHook extends IrcPluginHookBase implements IIrcPlugi
     }
 
 
+    /**
+     *  Scan all channels of the server on connect.
+     */
     @Override
     public void onConnect( final IrcBotProxy bot ) {
         ChannelInfoHandler handler = new ChannelInfoHandler() {
@@ -49,12 +87,38 @@ public class WhereisIrcPluginHook extends IrcPluginHookBase implements IIrcPlugi
     }
     
     
+    /**
+     *  Gets a list of users on given channel and updates info about their occurrences.
+     */
     private void scanChannel( String channel, IrcBotProxy bot ) {
+        log.debug("  Scanning channel; " + channel);
         Date now = new Date();
         User[] users = bot.getUsers( channel );
         for( User user : users ) {
             this.whereIsService.updateUserInfo( user, channel, now );
         }
+    }
+
+
+    private static final DateFormat DF_TIME = new SimpleDateFormat("HH:mm:ss");
+    private static final DateFormat DF_DATE = new SimpleDateFormat("YYYY-MM-dd");
+    
+    private String informAbout( String nick, List<SeenInfo> occurences ) {
+        Date now = new Date();
+        Date hours24Ago = DateUtils.addDays(now, -1);
+        
+        StringBuilder sb = new StringBuilder(" User " + nick + " was in ");
+        for (SeenInfo seenInfo : occurences) {
+            sb.append( seenInfo.userOrChannel );
+            sb.append( "(" );
+            
+            DateFormat df = (hours24Ago.before( seenInfo.when ) ? DF_TIME : DF_DATE);
+            synchronized( df ){
+                sb.append( df.format(seenInfo.when) );
+            }
+            sb.append( ")" );
+        }
+        return sb.toString();
     }
 
     
