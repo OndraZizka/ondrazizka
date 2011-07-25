a2a5ba36988aa2897d84b5932b144d010e607398
ondra@dynawest.cz <ondra@dynawest.cz@d2c96cac-2364-11df-9cb1-5138f70e336d>
2011-07-25 04:12:55 +0000

    Logger categ fixed

--- trunk/bots/JawaBot/branches/2.0/web/src/main/java/org/jboss/jawabot/web/_pg/UserPage.java
+++ trunk/bots/JawaBot/branches/2.0/web/src/main/java/org/jboss/jawabot/web/_pg/UserPage.java
@@ -19,7 +19,7 @@ import org.slf4j.LoggerFactory;
  */
 public class UserPage extends BaseLayoutPage
 {
-  private static final Logger log = LoggerFactory.getLogger( ResourcePage.class );
+  private static final Logger log = LoggerFactory.getLogger( UserPage.class );
 
   
    // Page params.
