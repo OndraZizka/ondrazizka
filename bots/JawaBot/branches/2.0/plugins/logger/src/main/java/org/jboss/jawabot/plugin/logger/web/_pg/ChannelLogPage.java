1cc2347fe1734f29ea9e6e52500580591c958b67
ondra <ondra@dynawest.cz>
2011-07-26 12:14:29 +0000

    Logger - added a page, Added reservation placeholder

---
+++ bots/JawaBot/branches/2.0/plugins/logger/src/main/java/org/jboss/jawabot/plugin/logger/web/_pg/ChannelLogPage.java
@@ -0,0 +1,52 @@
+
+package org.jboss.jawabot.plugin.logger.web._pg;
+
+
+import javax.inject.Inject;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.jboss.jawabot.plugin.irc.Channel;
+import org.jboss.jawabot.plugin.irc.ChannelManager;
+import org.jboss.jawabot.plugin.irc.web._co.ChannelDetailPanel;
+import org.jboss.jawabot.plugin.irc.web._co.ChannelListPanel;
+import org.jboss.jawabot.plugin.logger.web._co.ChannelLogPanel;
+import org.jboss.jawabot.web._base.BaseLayoutPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ *
+ * @author Ondrej Zizka
+ */
+public class ChannelLogPage extends BaseLayoutPage
+{
+  private static final Logger log = LoggerFactory.getLogger( ChannelLogPage.class );
+
+  @Inject private ChannelManager channelManager;
+  
+  
+  // Page params.
+  public final static String PARAM_NAME = "name";
+  
+  
+  /** Page const */
+  public ChannelLogPage( PageParameters params ) {
+     super(params);
+     log.debug(" Page params: " + params );
+
+     String name = params.getString(PARAM_NAME);
+     Channel chan = channelManager.byName( name );
+ 
+     if( chan != null ){
+        add( new Label("heading", "Channel "+chan.getName()+" - details"));
+        add( new ChannelLogPanel( "channelPanel", chan ) );
+     }
+     else{
+        add( new Label("heading", "Log"));
+        add( new ChannelListPanel("channelPanel") );
+     }
+  }
+
+
+}// class HomePage
