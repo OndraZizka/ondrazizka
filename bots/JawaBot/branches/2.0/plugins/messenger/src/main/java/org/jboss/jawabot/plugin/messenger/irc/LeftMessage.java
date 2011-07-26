bcda0ada0be3890810d592ecb93f2231175696c7
ondra <ondra@dynawest.cz>
2011-07-26 20:44:07 +0000

    Messenger plugin implemented, not tested.

--- bots/JawaBot/branches/2.0/plugins/messenger/src/main/java/org/jboss/jawabot/plugin/messenger/irc/LeftMessage.java
+++ bots/JawaBot/branches/2.0/plugins/messenger/src/main/java/org/jboss/jawabot/plugin/messenger/irc/LeftMessage.java
@@ -1,10 +1,8 @@
-package org.jboss.jawabot.plugin.whereis.irc;
+package org.jboss.jawabot.plugin.messenger.irc;
 
+import java.util.Date;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import org.jboss.jawabot.irc.ent.IrcEvMessage;
 
 /**
@@ -14,6 +12,20 @@ import org.jboss.jawabot.irc.ent.IrcEvMessage;
 @Entity
 @DiscriminatorValue("L")
 public class LeftMessage extends IrcEvMessage {
+
+		public LeftMessage() {
+		}
+
+		public LeftMessage(String server, String user, String channel, String text, Date when) {
+				super(server, user, channel, text, when);
+		}
+
+		// Conversion - IrcEvMessage -> LeftMessage.
+		public LeftMessage( IrcEvMessage msg ) {
+				this( msg.getServer(), msg.getUser(), msg.getChannel(), msg.getText(), msg.getWhen() );
+		}
+
+		
 
 }// class
 
