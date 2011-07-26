67e59f7b3748884a1fd48326109cc998e7a20b55
ondra@dynawest.cz <ondra@dynawest.cz@d2c96cac-2364-11df-9cb1-5138f70e336d>
2011-07-26 12:14:29 +0000

    Logger - added a page, Added reservation placeholder

---
+++ trunk/bots/JawaBot/branches/2.0/plugins/logger/src/main/java/org/jboss/jawabot/plugin/logger/web/_co/ChannelLogPanel.java
@@ -0,0 +1,48 @@
+
+package org.jboss.jawabot.plugin.logger.web._co;
+
+import javax.inject.Inject;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.util.ListModel;
+import org.jboss.jawabot.plugin.irc.Channel;
+import org.jboss.jawabot.plugin.irc.ChannelManager;
+
+/**
+ *  
+ *  @author Ondrej Zizka
+ */
+public class ChannelLogPanel extends Panel {
+   
+   @Inject private ChannelManager channelManager;
+   
+   public ChannelLogPanel( String id ) {
+      super( id );
+      super.setDefaultModel( new ListModel( channelManager.getChannelsRange_OrderByWhenDesc(0, 2000) ) ); // TBD
+   }
+
+   public ChannelLogPanel( String id, final Channel ch ) {
+      super( id, new Model<Channel>(ch) );
+   }
+
+   
+   
+   @Override
+   protected void onInitialize() {
+      super.onInitialize();
+
+        add(new ListView<IrcLogEntry>("messages", (ListModel) this.getDefaultModel() ) {
+           @Override protected void populateItem(ListItem<IrcLogEntry> item) {
+              item.add(new Label("nick", item.getModelObject() ));
+              item.add(new Label("msg", item.getModelObject() ));
+           }
+        });
+        
+   }
+   
+   
+   
+}
---
+++ trunk/bots/JawaBot/branches/2.0/plugins/reservation/src/main/java/org/jboss/jawabot/plugin/autoop/irc/AutoOpIrcPluginHook.java
@@ -0,0 +1,63 @@
+package org.jboss.jawabot.plugin.autoop.irc;
+
+import org.jboss.jawabot.irc.IIrcPluginHook;
+import org.jboss.jawabot.irc.IrcBotProxy;
+import org.jboss.jawabot.irc.IrcPluginHookBase;
+import org.jibble.pircbot.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *  Automatically ops everyone in the channel.
+ *  Also asks for operator if not having it.
+ * 
+ *  @author Ondrej Zizka
+ */
+public class AutoOpIrcPluginHook extends IrcPluginHookBase implements IIrcPluginHook<Object> {
+   private static final Logger log = LoggerFactory.getLogger( AutoOpIrcPluginHook.class );
+
+   
+   // IRC stuff.
+
+
+    @Override
+    public void onJoin( String channel, String user, IrcBotProxy bot  ) {
+        if( ! this.isCanHaveOperator( channel, user ) )
+            return;
+        bot.op( channel, user );
+    }
+
+
+    @Override
+    public void onBotJoinChannel( String channel, IrcBotProxy bot ) {
+        
+        User[] users = bot.getUsers( channel );
+        for( User user : users ) {
+            if( user.isOp() )
+                continue;
+            if( ! this.isCanHaveOperator( channel, user.getNick() ) )
+                continue;
+            bot.op( channel, user.getNick() );
+        }
+        
+    }
+    
+    
+
+
+    /**
+     *  TODO: Perhaps get this info from database?
+     */
+    private boolean isCanHaveOperator( String channel, String user ) {
+        return true;
+    }
+   
+   
+   
+   
+   
+   
+
+   
+}// class
+
