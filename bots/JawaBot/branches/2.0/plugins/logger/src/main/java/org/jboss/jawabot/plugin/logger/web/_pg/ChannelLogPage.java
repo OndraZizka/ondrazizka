d9ede52dae8ed2bd068c850b4fb25a00e19078c3
ondra <ondra@dynawest.cz>
2011-08-05 05:27:54 +0000

    Spring dumped.

--- bots/JawaBot/branches/2.0/plugins/logger/src/main/java/org/jboss/jawabot/plugin/logger/web/_pg/ChannelLogPage.java
+++ bots/JawaBot/branches/2.0/plugins/logger/src/main/java/org/jboss/jawabot/plugin/logger/web/_pg/ChannelLogPage.java
@@ -1,10 +1,12 @@
-
 package org.jboss.jawabot.plugin.logger.web._pg;
-
 
 import javax.inject.Inject;
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxCheckBox;
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.CheckBox;
+import org.apache.wicket.model.PropertyModel;
 import org.jboss.jawabot.plugin.irc.Channel;
 import org.jboss.jawabot.plugin.irc.ChannelManager;
 import org.jboss.jawabot.plugin.logger.web._co.ChannelLogPanel;
@@ -13,41 +15,60 @@ import org.jboss.jawabot.web._base.BaseLayoutPage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  *
  * @author Ondrej Zizka
  */
-public class ChannelLogPage extends BaseLayoutPage
-{
-  private static final Logger log = LoggerFactory.getLogger( ChannelLogPage.class );
+public class ChannelLogPage extends BaseLayoutPage {
 
-  @Inject private ChannelManager channelManager;
-  
-  
-  // Page params.
-  public final static String PARAM_NAME = "name";
-  
-  
-  /** Page const */
-  public ChannelLogPage( PageParameters params ) {
-     super(params);
-     log.debug(" Page params: " + params );
+    private static final Logger log = LoggerFactory.getLogger(ChannelLogPage.class);
+    
+    @Inject
+    private ChannelManager channelManager;
+    
+    // Page params.
+    public final static String PARAM_NAME = "name";
+    
+    // Page state.
+    private boolean onlyShowMessages = false;
 
-     String name = params.getString(PARAM_NAME);
-     Channel chan = name == null ? null : channelManager.byName( name );
- 
-     if( chan == null ){
-        log.debug("Channel is null, using ChannelListPanel.");
-        add( new Label("heading", "Logged Channels"));
-        add( new LoggedChannelsListPanel("channelLogPanel") );
-     }
-     else{
-        log.debug("Using ChannelLogPanel with channel: " +chan);
-        add( new Label("heading", "Channel "+chan.getName()+" - log"));
-        add( new ChannelLogPanel( "channelLogPanel", chan ) );
-     }
-  }
+    
+    
+    /** Page const */
+    public ChannelLogPage(PageParameters params) {
+        super(params);
+        log.debug(" Page params: " + params);
+
+        String name = params.getString(PARAM_NAME);
+        Channel chan = name == null ? null : channelManager.byName(name);
+
+        // Checkbox.
+        add(new AjaxCheckBox("onlyMessages", new PropertyModel<Boolean>(this, "onlyShowMessages")) {
+            protected void onUpdate(AjaxRequestTarget target) {
+                target.appendJavascript(" document.getElementById('channelLogPanel').className = 'onlyMessages';");
+            }
+        });
 
 
+        // Log table or channel list.
+        if (chan == null) {
+            log.debug("Channel is null, using ChannelListPanel.");
+            add(new Label("heading", "Logged Channels"));
+            add(new LoggedChannelsListPanel("channelLogPanel"));
+        } else {
+            log.debug("Using ChannelLogPanel with channel: " + chan);
+            add(new Label("heading", "Channel " + chan.getName() + " - log"));
+            add(new ChannelLogPanel("channelLogPanel", chan));
+        }
+    }
+
+    //<editor-fold defaultstate="collapsed" desc="get/set">
+    public boolean isOnlyShowMessages() {
+        return onlyShowMessages;
+    }
+
+    public void setOnlyShowMessages(boolean onlyShowMessages) {
+        this.onlyShowMessages = onlyShowMessages;
+    }
+    //</editor-fold>
 }// class HomePage
