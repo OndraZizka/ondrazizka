4f2a4d6163c8d821bab5120bebd9e0a6092904c7
dynawest@gmail.com <dynawest@gmail.com@d2c96cac-2364-11df-9cb1-5138f70e336d>
2011-08-07 14:17:28 +0000

    Command handler copied to reserv plugin and refactored.

---
+++ trunk/bots/JawaBot/branches/2.0/plugins/reservation/src/main/java/org/jboss/jawabot/plugin/reserv/irc/ReservationIrcPluginHook.java
@@ -0,0 +1,64 @@
+package org.jboss.jawabot.plugin.reserv.irc;
+
+import org.jboss.jawabot.irc.IIrcPluginHook;
+import org.jboss.jawabot.irc.IrcBotProxy;
+import org.jboss.jawabot.irc.IrcPluginHookBase;
+import org.jboss.jawabot.irc.ent.IrcEvJoin;
+import org.jibble.pircbot.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *  Automatically ops everyone in the channel.
+ *  Also asks for operator if not having it.
+ * 
+ *  @author Ondrej Zizka
+ */
+public class ReservationIrcPluginHook extends IrcPluginHookBase implements IIrcPluginHook<Object> {
+   private static final Logger log = LoggerFactory.getLogger( ReservationIrcPluginHook.class );
+
+   
+   // IRC stuff.
+
+
+    @Override
+    public void onJoin( IrcEvJoin event, IrcBotProxy bot ) {
+        if( ! this.isCanHaveOperator( event.getChannel(), event.getUser() ) )
+            return;
+        bot.op( event.getChannel(), event.getUser() );
+    }
+
+
+    @Override
+    public void onBotJoinChannel( String channel, IrcBotProxy bot ) {
+        
+        User[] users = bot.getUsers( channel );
+        for( User user : users ) {
+            if( user.isOp() )
+                continue;
+            if( ! this.isCanHaveOperator( channel, user.getNick() ) )
+                continue;
+            bot.op( channel, user.getNick() );
+        }
+        
+    }
+    
+    
+
+
+    /**
+     *  TODO: Perhaps get this info from database?
+     */
+    private boolean isCanHaveOperator( String channel, String user ) {
+        return true;
+    }
+   
+   
+   
+   
+   
+   
+
+   
+}// class
+
