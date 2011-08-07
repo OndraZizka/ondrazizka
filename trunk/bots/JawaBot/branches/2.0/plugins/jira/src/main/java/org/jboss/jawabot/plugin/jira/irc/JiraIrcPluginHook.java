42e31ed2fbfb63bf43dd73db877c4ea3b10803a8
dynawest@gmail.com <dynawest@gmail.com@d2c96cac-2364-11df-9cb1-5138f70e336d>
2011-08-07 13:54:54 +0000

    More Jira plugin cleanup

--- trunk/bots/JawaBot/branches/2.0/plugins/jira/src/main/java/org/jboss/jawabot/plugin/jira/irc/JiraIrcPluginHook.java
+++ trunk/bots/JawaBot/branches/2.0/plugins/jira/src/main/java/org/jboss/jawabot/plugin/jira/irc/JiraIrcPluginHook.java
@@ -31,18 +31,6 @@ public class JiraIrcPluginHook extends IrcPluginHookBase implements IIrcPluginHo
     private static final Logger log = LoggerFactory.getLogger( JiraIrcPluginHook.class );
 
 		
-	 // TODO: Move to JawaBot core.
-   private static final String VERSION;
-   static{
-      String version = null;
-      try { version = JiraIrcPluginHook.class.getPackage().getImplementationVersion(); } catch( Throwable ex ){ }
-      if( version == null ) version = "";
-      VERSION = StringUtils.defaultString( version, "" );
-   }
-	 
-	 private static final String PROJECT_DOC_URL = "(www TBD)";
-
-		
 		
    //final String JIRA_KEY_REGEX = "([A-Z]{2,}\\-[0-9]+)";
    private static final String JIRA_KEY_REGEX = "((?<![-_.A-Z])[A-Z]{$minChars,}-[0-9]++)(?!(-|\\.[0-9A-Za-z]))";
@@ -76,50 +64,50 @@ public class JiraIrcPluginHook extends IrcPluginHookBase implements IIrcPluginHo
     @Override
     public void onMessage( IrcEvMessage ev, IrcBotProxy bot ) throws IrcPluginException {
 
-				// Either process a command or search for Jira IDs.
-				boolean wasCommand = false;
+        // Either process a command or search for Jira IDs.
+        boolean wasCommand = false;
 
 
-				String msgNormalized = ev.getText().trim().toLowerCase();
+        String msgNormalized = ev.getText().trim().toLowerCase();
 
-				// Check for presence of bot nick prolog.
-				boolean startsWithBotNick = msgNormalized.startsWith(bot.getNick().toLowerCase());
+        // Check for presence of bot nick prolog.
+        boolean startsWithBotNick = msgNormalized.startsWith( bot.getNick().toLowerCase() );
 
-				// If the prolog is present,
-				if( startsWithBotNick ){
+        // If the prolog is present,
+        if( startsWithBotNick ) {
 
-					// remove it,
-					int prologEnd = bot.getNick().length();
+            // remove it,
+            int prologEnd = bot.getNick().length();
 
-					String command = msgNormalized.substring( prologEnd );
-					command = StringUtils.removeStart(command, ":").trim();
+            String command = msgNormalized.substring( prologEnd );
+            command = StringUtils.removeStart( command, ":" ).trim();
 
-					// and process the command.
-					wasCommand = handleJiraBotCommand( ev.getChannel(), command, false, bot );
+            // and process the command.
+            wasCommand = handleJiraBotCommand( ev.getChannel(), command, false, bot );
 
-				}
+        }
 
 
-				// Not a command?  Search for Jira IDs.
-				if( !wasCommand ){
-					this.handleJiraRequest( ev.getChannel(), ev.getText().trim(), bot );
-				}
+        // Not a command?  Search for Jira IDs.
+        if( !wasCommand ) {
+            this.handleJiraRequest( ev.getChannel(), ev.getText().trim(), bot );
+        }
 
-				// TODO: Perhaps create some class for request info, carrying information like
-				//       isPrivateMsg, from, isCommand, commandName, command details, list of jiras found, etc.
-				//       parse the message into it's object, and then pass such object to the handlers.
-				//       (We don't need such sophisticated approach now :)
+        // TODO: Perhaps create some class for request info, carrying information like
+        //       isPrivateMsg, from, isCommand, commandName, command details, list of jiras found, etc.
+        //       parse the message into it's object, and then pass such object to the handlers.
+        //       (We don't need such sophisticated approach now :)
 
     }
 
 
 
-		@Override
-		public void onPrivateMessage(IrcEvMessage ev, IrcBotProxy bot) throws IrcPluginException {
-				this.handleJiraBotCommand( ev.getUser(), ev.getText().trim(), true, bot );
-		}
-		
-		
+    @Override
+    public void onPrivateMessage(IrcEvMessage ev, IrcBotProxy bot) throws IrcPluginException {
+            this.handleJiraBotCommand( ev.getUser(), ev.getText().trim(), true, bot );
+    }
+
+
 
 		
 		
@@ -132,17 +120,17 @@ public class JiraIrcPluginHook extends IrcPluginHookBase implements IIrcPluginHo
     * If there are more IDs than MAX_JIRA_IDS_PER_REQUEST, doesn't reply.
     *
     * @param from     Who did this message come from - user or #channel
-    * @param request
+    * @param msgText
     *
     * TODO: Very ugly code, refactor.
     */
-   void handleJiraRequest(String from, String request, IrcBotProxy bot ) {
-
+   void handleJiraRequest( String from, String msgText, IrcBotProxy bot )
+   {
       // Parse the JIRA IDs.
       List<String> jiraIDs = new ArrayList(); // List to keep the order of issues.
-      Matcher jiraIdMatcher = JIRA_KEY_PATTERN.matcher(request);
-      while (jiraIdMatcher.find()) {
-         jiraIDs.add(jiraIdMatcher.group().toUpperCase());
+      Matcher jiraIdMatcher = JIRA_KEY_PATTERN.matcher( msgText );
+      while( jiraIdMatcher.find() ) {
+         jiraIDs.add( jiraIdMatcher.group().toUpperCase() );
       }
 
       // No issues found.
@@ -156,28 +144,20 @@ public class JiraIrcPluginHook extends IrcPluginHookBase implements IIrcPluginHo
 
       boolean isChannel = from.startsWith("#");
       // TODO: Make this configurable.
-      boolean skipCache = request.contains("refresh") 
-                       || request.contains("nocache")
-                       || request.contains("resolved") 
-                       || request.contains("closed") 
-                       || request.contains("fixed") 
-                       || request.contains("reopened") 
-                       || request.contains("assigned");
-      boolean noURL     = request.contains("nourl");
-      long curTimeMS = System.currentTimeMillis();
-
-      // Show after at least 15 messages and 3 minutes.
-      // TODO: Make this configurable.
-      IssueShownInfo repeatedShowThreshold = new IssueShownInfo( 
-              this.config.settings.repeatDelayMessages, 
-              this.config.settings.repeatDelaySeconds * 1000 );
-
+      boolean skipCache = msgText.contains("refresh") 
+                       || msgText.contains("nocache")
+                       || msgText.contains("resolved") 
+                       || msgText.contains("closed") 
+                       || msgText.contains("fixed") 
+                       || msgText.contains("reopened") 
+                       || msgText.contains("assigned");
+      boolean noURL     = msgText.contains("nourl");
 
       // For each JIRA ID found...
       for( String issueID : jiraIDs )
       {
-         this.handleIssueId( issueID, from, request, isChannel, skipCache, noURL, curTimeMS, repeatedShowThreshold, bot );
-      }// for each issue ID found.
+         this.handleIssueId( issueID, from, msgText, isChannel, skipCache, noURL, bot );
+      }
    }// handleJiraRequest()
 
 
@@ -190,35 +170,41 @@ public class JiraIrcPluginHook extends IrcPluginHookBase implements IIrcPluginHo
     */
    private void handleIssueId( String issueID, String from, String request,
            boolean isChannel, boolean skipCache, boolean noURL,
-           long curTimeMS, IssueShownInfo repeatedShowThreshold, IrcBotProxy bot
+           IrcBotProxy bot
    ) {
 
-      // TODO: Refactor - repeated in other method.
-      String replyTo = this.config.settings.debug ? this.config.settings.debugChannel : from;
+        // TODO: Refactor - repeated in other method.
+        String replyTo = this.config.settings.debug ? this.config.settings.debugChannel : from;
 
-      // Skip Jiras with ignored prefixes.
-      if( this.repoManager.hasIgnoredPrefix(issueID) )
-         return;
-
-      IssueInfo issue = null;
-
-      
-      // DONE: Rewritten using RepositoriesManager and Scrapers.
-      {
-         // Check the channel activity - when this issue was last shown.
-         if( isChannel && this.channelStatusStore.isTooEarlyToRepeat( from, issueID, curTimeMS, repeatedShowThreshold ) )
+        // Skip Jiras with ignored prefixes.
+        if( this.repoManager.hasIgnoredPrefix(issueID) )
             return;
 
-         // Update the channel info.
-         if( isChannel )
+        IssueInfo issue = null;
+
+      
+
+        // Show after at least 15 messages and 3 minutes.
+        // TODO: Make this configurable.
+        IssueShownInfo repeatedShowThreshold = new IssueShownInfo( 
+                 this.config.settings.repeatDelayMessages, 
+                 this.config.settings.repeatDelaySeconds * 1000 );
+        long curTimeMS = System.currentTimeMillis();
+
+        // Check the channel activity - when this issue was last shown.
+        if( isChannel && this.channelStatusStore.isTooEarlyToRepeat( from, issueID, curTimeMS, repeatedShowThreshold ) )
+            return;
+
+        // Update the channel info.
+        if( isChannel )
             this.channelStatusStore.onIssueAnnounced( from, issueID );
 
 
-         // Check the cache.
-         issue = this.issueCache.getItem( issueID );
-         log.debug("  Cached issue info: " + issue);
+        // Check the cache.
+        issue = this.issueCache.getItem( issueID );
+        log.debug("  Cached issue info: " + issue);
 
-         if( issue == null ){
+        if( issue == null ){
             RepositoryBean repo = this.repoManager.getRepoForIssue(issueID);
             log.debug("Repo for this issue: " + repo);
             if( null == repo ) return;
@@ -236,112 +222,71 @@ public class JiraIrcPluginHook extends IrcPluginHookBase implements IIrcPluginHo
                bot.sendMessage( replyTo, ex.getMessage() );
                return;
             }
-         }
+        }
 
-         // If the request contains the URL, don't repeat it.
-         String urlPart = StringUtils.substringAfter( issue.getUrl(), "//");
-         boolean noURLcur = noURL || request.contains( urlPart );
+        // If the request contains the URL, don't repeat it.
+        String urlPart = StringUtils.substringAfter( issue.getUrl(), "//");
+        boolean noURLcur = noURL || request.contains( urlPart );
 
-         // Create and send the message.
-         String reply = String.format( noURLcur ? "%s" : "%s %s",
+        // Create and send the message.
+        String reply = String.format( noURLcur ? "%s" : "%s %s",
             this.formatResponse( issue ), issue.getUrl() );
-         bot.sendMessage( replyTo, reply );
+        bot.sendMessage( replyTo, reply );
 
-         // Update the cache.
-         this.issueCache.putItem( issueID, issue );
+        // Update the cache.
+        this.issueCache.putItem( issueID, issue );
 
-      }
-
-   }// handleIssueId()
+    }// handleIssueId()
 
 
 
- 		/**
-		 * Formats the string for response for the given issue.
-		 * @returns  "[#TOOLS-102] Implement Bugzilla support [Open, Major, Ondrej Zizka]
-		 */
-		public String formatResponse(IssueInfo issue) {
-				String reply = String.format("[#%s] %s [%s, %s, %s]",
-								issue.getId(),
-								issue.getTitle(),
-								issue.getStatus(), issue.getPriority(), issue.getAssignedTo());
-				return reply;
-		}
+    /**
+     * Formats the string for response for the given issue.
+     * @returns  "[#TOOLS-102] Implement Bugzilla support [Open, Major, Ondrej Zizka]
+     */
+    public String formatResponse(IssueInfo issue) {
+            String reply = String.format("[#%s] %s [%s, %s, %s]",
+                                            issue.getId(),
+                                            issue.getTitle(),
+                                            issue.getStatus(), issue.getPriority(), issue.getAssignedTo());
+            return reply;
+    }
 
-		
-		/**
-		 * Handles a command, which is (assumably) sent as PM to the bot.
-		 *
-		 * @param replyTo     Nick or channel (#...) from which the message was received.
-		 * @param command  The command - the relevant part - i.e. ignoring
-		 *                 "jirabot" etc at the beginning of the messagge.
-		 *
-		 * @returns  true if the request was valid JiraBot command.
-		 */
-		boolean handleJiraBotCommand(String from, String command, boolean isFromPrivateMessage, IrcBotProxy bot) {
 
-				boolean wasValidCommand = false;
+    /**
+     * Handles a command, which is (assumably) sent as PM to the bot.
+     *
+     * @param replyTo     Nick or channel (#...) from which the message was received.
+     * @param command  The command - the relevant part - i.e. ignoring
+     *                 "jirabot" etc at the beginning of the messagge.
+     *
+     * @returns  true if the request was valid JiraBot command.
+     */
+    boolean handleJiraBotCommand( String from, String command, boolean isFromPrivateMessage, IrcBotProxy bot ) {
 
-				String replyTo = this.config.settings.debug ? this.config.settings.debugChannel : from;
+        boolean wasValidCommand = false;
 
-				command = command.toLowerCase();
+        String replyTo = this.config.settings.debug ? this.config.settings.debugChannel : from;
 
-				// Join a channel.
-				if (command.startsWith("join")) {
-						wasValidCommand = true;
+        command = command.toLowerCase();
 
-						if (!this.config.settings.allowJoinCommand) {
-								bot.sendMessage(replyTo, "join command not allowed - use /invite (if you are an op) or allow in JiraBot settings.");
-						} else {
-								String channel = command.substring(4).trim();
-								if (!channel.startsWith("#")) {
-										channel = "#" + channel;
-								}
-								if (!StringUtils.isAlphanumeric(channel.substring(1))) {
-										bot.sendMessage(replyTo, "Invalid channel name - must be alphanumeric: " + channel);
-								} else {
-										bot.sendMessage(replyTo, "Joining channel: " + channel);
-										bot.joinChannel(channel);
-								}
-						}
-				}
-				// Clear cache.
-				else if (command.startsWith("clearcache")) {
-						wasValidCommand = true;
-						String clearJiraID = command.substring(10).trim().toUpperCase();
-						if ("".equals(clearJiraID)) {
-								bot.sendMessage(replyTo, "Clearing the cache.");
-								this.issueCache.clear();
-						} else {
-								bot.sendMessage(replyTo, "Removing " + clearJiraID + " from the cache.");
-								this.issueCache.removeItem(clearJiraID);
-						}
-				}
-				// About or Help.
-				else if (command.startsWith("about jira") || command.startsWith("help jira")) {
-						wasValidCommand = true;
-						bot.sendMessage(replyTo,
-										"Hi, I'm a bot which brings some useful info about JIRA issues to IRC channels. Version: " + VERSION);
-						bot.sendMessage(replyTo,
-										"If you want me in your channel, invite me, usually done by typing '/invite " + bot.getNick() + "' in that channel.");
-						bot.sendMessage(replyTo,
-										"If you don't like me, kick me off. Or say 'jirabot please leave'.");
-						bot.sendMessage(replyTo,
-										"For more info, see " + PROJECT_DOC_URL);
-				}
-			  // Specially for Rado:  Thanks.
-				else if (command.startsWith("thank")) {
-						wasValidCommand = true;
-						bot.sendMessage(replyTo, "yw");
-				}
-				else if (command.contains("good")) {
-						wasValidCommand = true;
-						bot.sendMessage(replyTo, "Oh yes indeed.");
-				}
+        // Clear cache.
+        if (command.startsWith("clearcache")) {
+            wasValidCommand = true;
+            String clearJiraID = command.substring(10).trim().toUpperCase();
+            if ("".equals(clearJiraID)) {
+                bot.sendMessage(replyTo, "Clearing the cache.");
+                this.issueCache.clear();
+            } else {
+                bot.sendMessage(replyTo, "Removing " + clearJiraID + " from the cache.");
+                this.issueCache.removeItem(clearJiraID);
+            }
+        }
 
-				return wasValidCommand;
+        return wasValidCommand;
 
-		}// handleJiraBotCommand()
+    }// handleJiraBotCommand()
+
 
 }// class
 
